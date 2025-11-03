b938f322c3ffea0949402dccb382d0c46ea57158
nakedcow <111654579+nakedcow@users.noreply.github.com>
2025-11-02 18:27:20 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "pig-scorekeeper-v17";
+const CACHE = "pig-scorekeeper-v18";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./favicon-32.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))); self.clients.claim(); });
